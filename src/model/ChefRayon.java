99e5b6408818b7d00aaf179fbd1a3b61ffc2dd25
Jean Urbain Koku Dziedzom KUMAKO <jeanur191207@gmail.com>
2026-07-03 23:12:29 +0000

    Classe Rayon complète (attributs, constructeur, getters/setters, calculerValeurStock, toString) + préparation de la classe Chef rayon qui sera fait par l'autre membre du groupe pour le TP

---
+++ src/model/ChefRayon.java
@@ -0,0 +1,14 @@
+package model;
+
+
+//TODO : Classe à compléter par [Nom du camarade - Personne 2]
+//Attributs prévus (voir diagramme de classes) :
+//numeroCaisse, ouvrirCaisse(), effectuerVente() -> non, ceci est pour Caissier
+//Pour ChefRayon : ajouterProduit(), retirerProduit()
+//Hérite de Employe (attributs communs : matricule, nom, prenom, salaire)
+
+
+
+public class ChefRayon {
+
+}
--- src/model/Rayon.java
+++ src/model/Rayon.java
@@ -1,6 +1,68 @@
 package model;
 
+import java.util.ArrayList;
 //Classe représentant un rayon du supermarché
 public class Rayon {
 
+	// Déclaration des champs (attributs de la classe Rayon)
+	private int codeRayon;
+	private String nomRayon;
+	private ChefRayon responsable;
+	private ArrayList<Produit> listeProduits;
+	
+	// Constructeur d'initialisation
+	public Rayon(int codeRayon, String nomRayon, ChefRayon responsable) {
+	    this.codeRayon = codeRayon;
+	    this.nomRayon = nomRayon;
+	    this.responsable = responsable;
+	    this.listeProduits = new ArrayList<Produit>();
+	}
+	// Accès et modification du code du rayon
+	public int getCodeRayon() {
+		return codeRayon;
+	}
+
+	public void setCodeRayon(int codeRayon) {
+		this.codeRayon = codeRayon;
+	}
+	// Accès et modification du nom du rayon
+	public String getNomRayon() {
+		return nomRayon;
+	}
+
+	public void setNomRayon(String nomRayon) {
+		this.nomRayon = nomRayon;
+	}
+	// Accès et modification du responsable du rayon
+	public ChefRayon getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(ChefRayon responsable) {
+		this.responsable = responsable;
+	}
+	// Accès et modification de la liste des produits du rayon
+	public ArrayList<Produit> getListeProduits() {
+		return listeProduits;
+	}
+
+	public void setListeProduits(ArrayList<Produit> listeProduits) {
+		this.listeProduits = listeProduits;
+	};
+	
+	// Calcule la valeur totale du stock des produits présents dans le rayon
+	public double calculerValeurStock() {
+	    double valeurTotale = 0;
+	    for (int i = 0; i < this.listeProduits.size(); i++) {
+	        Produit produit = this.listeProduits.get(i);
+	        valeurTotale = valeurTotale + (produit.getPrixVente() * produit.getQuantiteStock());
+	    }
+	    return valeurTotale;
+	}
+	@Override
+	public String toString() {
+		return "Rayon [codeRayon=" + codeRayon + ", nomRayon=" + nomRayon + ", responsable=" + responsable
+				+ ", listeProduits=" + listeProduits + "]";
+	}
+	
 }
